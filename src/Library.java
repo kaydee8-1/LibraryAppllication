6ec411aa2921c495552b8b11575804a837a76d96
Kaja Drozd <drozd.kaja.pasp1@gmail.com>
2023-09-24 14:34:44 +0200

    Initial commit

---
+++ src/Library.java
@@ -0,0 +1,7 @@
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Gdzie śpiewają raki", "Delia Owens", 2018, 410, "Świat Książki", "123456789");
+
+        book1.printInfo();
+    }
+}
